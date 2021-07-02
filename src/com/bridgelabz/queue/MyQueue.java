0fef477e5d8123b2bb58be9710f1852ec819d58a
nithin-sudo <nithinkrishnasathram@gmail.com>
2021-07-02 12:21:01 +0530

    Added:enQueue method to insert node in the queue.

--- src/com/bridgelabz/queue/MyQueue.java
+++ src/com/bridgelabz/queue/MyQueue.java
@@ -1,7 +1,38 @@
 package com.bridgelabz.queue;
 
 public class MyQueue {
-    public static void main(String[] args) {
-        System.out.println("Welcome to queue using linked list");
+    Node head;
+    Node tail;
+    /**
+     * insertring the elements in the queue
+     * @param data
+     * @param <T>
+     */
+    public<T> void enQueue(T data)
+    {
+        Node newNode = new Node();
+        newNode.data=data;
+        newNode.next=null;
+        if(head == null && tail == null)
+        {
+            head = tail = newNode;
+        }
+        else{
+            tail.next=newNode;
+            tail=newNode;
+        }
+    }
+    /**
+     * printing the queue elements.
+     */
+    public void printQueue()
+    {
+        Node tempNode = head;
+        System.out.println("the elements in the queue is :");
+        while(tempNode!=null)
+        {
+            System.out.println(tempNode.data);
+            tempNode=tempNode.next;
+        }
     }
 }
--- src/com/bridgelabz/queue/Runner.java
+++ src/com/bridgelabz/queue/Runner.java
@@ -1,4 +1,11 @@
 package com.bridgelabz.queue;
 
 public class Runner {
+    public static void main(String[] args) {
+        MyQueue myQueue = new MyQueue();
+        myQueue.enQueue(55);
+        myQueue.enQueue(30);
+        myQueue.enQueue(70);
+        myQueue.printQueue();
+    }
 }
